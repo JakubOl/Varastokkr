5430bad5f7e5c5794d9f166bcef4e2bc7e81aab6
Jakub <jkb.olbrycht@gmail.com>
2025-01-29 21:43:13 +0100

    deploy identityapi update to azure containers, test endpoint

---
+++ Varastokkr.IdentityAPI/Endpoints/TestEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Varastokkr.Shared.Abstract;
+
+namespace Varastokkr.IdentityAPI.Endpoints;
+
+internal class TestEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("echo", () =>
+                {
+                    
+                    return Results.Ok("ECHO");
+                })
+            .Produces(StatusCodes.Status200OK)
+            .WithName("Echo")
+            .WithOpenApi(operation =>
+            {
+                operation.Summary = "Echo endpoint";
+                operation.Description = "Echo";
+                return operation;
+            });
+    }
+}
